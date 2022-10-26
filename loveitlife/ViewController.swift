bd4c470ae9dc7ba8fca99cc6ff3d860b87e77f1c
Patrick <tasniim.bari7@gmail.com>
2022-10-26 14:54:21 +0100

    initial commit

--- loveitlife/ViewController.swift
+++ loveitlife/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label1.text = "Hello There"
     }
 
 
